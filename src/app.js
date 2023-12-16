21b9c6bc20129034cfdb3aa080833b840be05433
fomes <fgomesdeluna@gmail.com>
2023-12-16 12:51:37 -0300

    products crud complete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 const connectDB = require("./config/database");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -13,6 +14,8 @@ connectDB();
 app.use("/hello", (req, res) => {
   res.json({ message: "Hello API!" });
 });
+
+app.use("/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/controllers/productController.js
@@ -0,0 +1,46 @@
+const Product = require("../models/Product");
+
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+};
